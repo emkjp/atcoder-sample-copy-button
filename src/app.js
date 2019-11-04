fb36ad69c91220e53c5c5b2be4e69093dce03b02
EmK <emkjp@users.noreply.github.com>
2019-11-04 13:48:31 +0900

    Care old atcoder pages

--- src/app.js
+++ src/app.js
@@ -8,64 +8,48 @@ function copyToClipboard(text) {
     var parent = textarea.parentElement;
     parent.removeChild(textarea);
 }
-function extractSampleInputs() {
+function extractSampleInputsAndOutputs() {
     var a = document.getElementsByClassName('lang-ja') ||
         document.getElementsByClassName('lang-en');
-    var preElements = a[0].getElementsByTagName('pre');
-    var allSampleInput = [];
+    var preElements = a.length > 0
+        ? a[0].getElementsByTagName('pre')
+        : document.getElementsByTagName('pre');
+    var allSamples = [];
     for (var i = 0; i < preElements.length; i++) {
         var e = preElements[i];
         if (e.id.indexOf('pre-sample') === 0) {
-            var nth = parseInt(e.id.substr('pre-sample'.length));
-            if (nth % 2 === 0) {
-                allSampleInput.push(e.innerText);
-            }
+            allSamples.push(e.innerText);
         }
     }
-    return allSampleInput;
-}
-function extractSampleOutputs() {
-    var a = document.getElementsByClassName('lang-ja') ||
-        document.getElementsByClassName('lang-en');
-    var preElements = a[0].getElementsByTagName('pre');
-    var allSampleOutput = [];
-    for (var i = 0; i < preElements.length; i++) {
-        var e = preElements[i];
-        if (e.id.indexOf('pre-sample') === 0) {
-            var nth = parseInt(e.id.substr('pre-sample'.length));
-            if (nth % 2 === 1) {
-                allSampleOutput.push(e.innerText);
-            }
-        }
-    }
-    return allSampleOutput;
-}
-function copyAllSampleInputToClipBoard() {
-    copyToClipboard(extractSampleInputs().join('\n'));
-}
-function copyAllSampleOutputToClipBoard() {
-    copyToClipboard(extractSampleOutputs().join('\n'));
+    return allSamples;
 }
 function insertNextTo(newChild, refChild) {
     if (refChild.parentNode === null)
         return;
     refChild.parentNode.insertBefore(newChild, refChild.nextSibling.nextSibling);
 }
-function addButtonToCopyAllSampleInput() {
+function addButtonToCopyAllSampleInput(sampleInputs, sampleOutputs) {
     var ioStyle = document.getElementsByClassName('io-style');
     for (var i = 0; i < ioStyle.length; i++) {
         var inputButton = document.createElement('span');
         inputButton.className = 'btn btn-default btn-sm btn-copy';
         inputButton.innerText = 'Copy All sample inputs';
-        inputButton.onclick = copyAllSampleInputToClipBoard;
+        inputButton.onclick = function () { return copyToClipboard(sampleInputs.join('\n')); };
         var outputButton = document.createElement('span');
         outputButton.className = 'btn btn-default btn-sm btn-copy ml-1';
         outputButton.innerText = 'Copy All sample outputs';
-        outputButton.onclick = copyAllSampleOutputToClipBoard;
+        outputButton.onclick = function () {
+            return copyToClipboard(sampleOutputs.join('\n'));
+        };
         var hr = document.createElement('hr');
         insertNextTo(outputButton, ioStyle[i]);
         insertNextTo(inputButton, ioStyle[i]);
         insertNextTo(hr, ioStyle[i]);
     }
 }
-addButtonToCopyAllSampleInput();
+var samples = extractSampleInputsAndOutputs();
+if (samples.length > 0) {
+    var sampleInputs = samples.filter(function (s, index) { return index % 2 == 0; });
+    var sampleOutputs = samples.filter(function (s, index) { return index % 2 == 1; });
+    addButtonToCopyAllSampleInput(sampleInputs, sampleOutputs);
+}
--- src/app.ts
+++ src/app.ts
@@ -8,56 +8,26 @@ function copyToClipboard(text: string): void {
   parent.removeChild(textarea);
 }
 
-function extractSampleInputs(): string[] {
+function extractSampleInputsAndOutputs(): string[] {
   const a =
     document.getElementsByClassName('lang-ja') ||
     document.getElementsByClassName('lang-en');
 
-  const preElements = (a[0] as HTMLSpanElement).getElementsByTagName('pre');
+  const preElements =
+    a.length > 0
+      ? (a[0] as HTMLSpanElement).getElementsByTagName('pre')
+      : document.getElementsByTagName('pre');
 
-  const allSampleInput = [];
-
-  for (let i = 0; i < preElements.length; i++) {
-    const e = preElements[i] as HTMLPreElement;
-    if (e.id.indexOf('pre-sample') === 0) {
-      const nth = parseInt(e.id.substr('pre-sample'.length));
-      if (nth % 2 === 0) {
-        allSampleInput.push(e.innerText);
-      }
-    }
-  }
-
-  return allSampleInput;
-}
-
-function extractSampleOutputs(): string[] {
-  const a =
-    document.getElementsByClassName('lang-ja') ||
-    document.getElementsByClassName('lang-en');
-
-  const preElements = (a[0] as HTMLSpanElement).getElementsByTagName('pre');
-
-  const allSampleOutput = [];
+  const allSamples = [];
 
   for (let i = 0; i < preElements.length; i++) {
     const e = preElements[i] as HTMLPreElement;
     if (e.id.indexOf('pre-sample') === 0) {
-      const nth = parseInt(e.id.substr('pre-sample'.length));
-      if (nth % 2 === 1) {
-        allSampleOutput.push(e.innerText);
-      }
+      allSamples.push(e.innerText);
     }
   }
 
-  return allSampleOutput;
-}
-
-function copyAllSampleInputToClipBoard(): void {
-  copyToClipboard(extractSampleInputs().join('\n'));
-}
-
-function copyAllSampleOutputToClipBoard(): void {
-  copyToClipboard(extractSampleOutputs().join('\n'));
+  return allSamples;
 }
 
 function insertNextTo(newChild: HTMLElement, refChild: HTMLElement): void {
@@ -68,18 +38,22 @@ function insertNextTo(newChild: HTMLElement, refChild: HTMLElement): void {
   );
 }
 
-function addButtonToCopyAllSampleInput(): void {
+function addButtonToCopyAllSampleInput(
+  sampleInputs: string[],
+  sampleOutputs: string[]
+): void {
   const ioStyle = document.getElementsByClassName('io-style');
   for (let i = 0; i < ioStyle.length; i++) {
     const inputButton = document.createElement('span') as HTMLSpanElement;
     inputButton.className = 'btn btn-default btn-sm btn-copy';
     inputButton.innerText = 'Copy All sample inputs';
-    inputButton.onclick = copyAllSampleInputToClipBoard;
+    inputButton.onclick = (): void => copyToClipboard(sampleInputs.join('\n'));
 
     const outputButton = document.createElement('span') as HTMLSpanElement;
     outputButton.className = 'btn btn-default btn-sm btn-copy ml-1';
     outputButton.innerText = 'Copy All sample outputs';
-    outputButton.onclick = copyAllSampleOutputToClipBoard;
+    outputButton.onclick = (): void =>
+      copyToClipboard(sampleOutputs.join('\n'));
 
     const hr = document.createElement('hr');
     insertNextTo(outputButton, ioStyle[i] as HTMLElement);
@@ -88,4 +62,9 @@ function addButtonToCopyAllSampleInput(): void {
   }
 }
 
-addButtonToCopyAllSampleInput();
+const samples = extractSampleInputsAndOutputs();
+if (samples.length > 0) {
+  const sampleInputs = samples.filter((s, index) => index % 2 == 0);
+  const sampleOutputs = samples.filter((s, index) => index % 2 == 1);
+  addButtonToCopyAllSampleInput(sampleInputs, sampleOutputs);
+}
